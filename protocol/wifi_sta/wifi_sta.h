051f256142d56787f161576d47bb47f2f8adb444
mmmmmmore <maochuneric@gmail.com>
2025-11-28 12:46:19 +0800

    declaration of sta wifi

--- protocol/wifi_sta/wifi_sta.h
+++ protocol/wifi_sta/wifi_sta.h
@@ -1,11 +1,12 @@
-#ifndef WIFIAP_H
-#define WIFIAP_H
+#ifndef WIFI_STA_H
+#define WIFI_STA_H
 
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
+#include "esp_err.h"
 
-// 初始化 WiFi SoftAP
-void wifi_init_softap(void);
+// 初始化并连接到指定的GW SSID/密码
+esp_err_t wifi_sta_init(const char *ssid, const char *password);
 
-#endif // WIFIAP_H
+// 获取当前连接状态
+bool wifi_sta_is_connected(void);
+
+#endif // WIFI_STA_H
