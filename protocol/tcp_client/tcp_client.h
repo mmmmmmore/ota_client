5c678699ef3fd2347e484d9f55d31327fe19b79d
mmmmmmore <maochuneric@gmail.com>
2025-11-28 13:02:46 +0800

    initiate version of tcp client

---
+++ protocol/tcp_client/tcp_client.h
@@ -0,0 +1,15 @@
+#ifndef TCP_CLIENT_H
+#define TCP_CLIENT_H
+
+#include "esp_err.h"
+
+// 初始化 TCP 客户端并连接到 GW
+esp_err_t tcp_client_start(const char *gw_ip, uint16_t gw_port);
+
+// 发送 JSON 消息到 GW
+esp_err_t tcp_client_send(const char *json_str);
+
+// 接收 GW 下发的任务（阻塞或事件回调）
+void tcp_client_task(void *pvParameters);
+
+#endif // TCP_CLIENT_H
