2632686da4b86b17989613bac46410169c1ca4d6
mmmmmmore <maochuneric@gmail.com>
2025-12-02 16:37:57 +0800

    add tcp_client_set_receive_callback function

--- protocol/tcp_client/tcp_client.h
+++ protocol/tcp_client/tcp_client.h
@@ -1,17 +1,20 @@
-#ifndef TCP_CLIENT_H
-#define TCP_CLIENT_H
-
+#pragma once
 #include "esp_err.h"
 
-// 初始化 TCP 客户端并连接到 GW
+// 定义接收回调函数类型
+typedef void (*tcp_receive_cb_t)(const char *data, size_t len);
+
+// 设置接收回调
+void tcp_client_set_receive_callback(tcp_receive_cb_t cb);
+
+// 启动 TCP 客户端
 esp_err_t tcp_client_start(const char *gw_ip, uint16_t gw_port);
 
-// 发送 JSON 消息到 GW
+// 发送数据
 esp_err_t tcp_client_send(const char *json_str);
 
+// 获取当前 socket
 int tcp_client_get_sock(void);
 
-// 接收 GW 下发的任务（阻塞或事件回调）
+// TCP 客户端任务
 void tcp_client_task(void *pvParameters);
-
-#endif // TCP_CLIENT_H
