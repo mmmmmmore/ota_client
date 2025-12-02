c32db0a4d794f608f26cd15ba62d5052353ac949
mmmmmmore <maochuneric@gmail.com>
2025-12-02 16:41:25 +0800

    Update msg_handler.h

--- handler/msg_handler/msg_handler.h
+++ handler/msg_handler/msg_handler.h
@@ -4,6 +4,9 @@
 #include "esp_err.h"
 
 // 处理从 GW 收到的消息
-esp_err_t msg_handler_process(const char *json_str);
+// msg_handler.h
+void msg_handler_process(const char *json_str, size_t len);
+
 
 #endif // MSG_HANDLER_H
+
